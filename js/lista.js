fb6520b274c08d7ff16a0108146c84869a8303ac
AlessandroAraujo <alessandro.aoc@gmail.com>
2022-04-27 15:03:22 -0400

    penas modificações

--- js/lista.js
+++ js/lista.js
@@ -1,5 +1,8 @@
+
+
 function filme01() {
-    document.getElementById("titulo").innerHTML = "batman";
+    document.getElementById('titulo')
+        .innerHTML = "The Batman";
     document.getElementById("descricao")
         .innerHTML = "Batman (The Batman, no original) segue o segundo ano de Bruce Wayne (Robert Pattinson) como o herói de Gotham, causando medo nos corações dos criminosos da sombria cidade."
 
@@ -7,11 +10,83 @@ function filme01() {
 }
 
 function filme02() {
-    document.getElementById("titulo").innerHTML = "O Projeto Adam";
+    document.getElementById("titulo")
+        .innerHTML = "O Projeto Adam";
     document.getElementById("descricao")
         .innerHTML = "Um viajante do tempo volta ao ano de 2022 por acidente e acaba se encontrando com seu eu mais novo. Juntos, e sendo caçados por forças do futuro, os dois embarcam em uma missão para consertar a linha temporal e salvar seus entes queridos."
 
 }
+
+function filme03() {
+    document.getElementById("titulo")
+        .innerHTML = "Better Call Saul";
+    document.getElementById("descricao")
+        .innerHTML = "Os julgamentos e atribulações do advogado criminal, Jimmy McGill, e o estabelecimento de seu escritório de advocacia em Albuquerque, Novo México."
+
+}
+
+function filme04() {
+    document.getElementById("titulo")
+        .innerHTML = "RED ";
+    document.getElementById("descricao")
+        .innerHTML = "Uma jovem vive um ano de formação na companhia de um enorme panda vermelho."
+
+}
+
+function filme05() {
+    document.getElementById("titulo")
+        .innerHTML = "The Good Doctor";
+    document.getElementById("descricao")
+        .innerHTML = "Shaun Murphy, um jovem cirurgião com autismo e síndrome de Savant, é recrutado na unidade cirúrgica de um hospital de prestígio."
+
+}
+
+function filme06() {
+    document.getElementById("titulo")
+        .innerHTML = "Homem-Aranha: Sem Volta para Casa";
+    document.getElementById("descricao")
+        .innerHTML = "Com a identidade do Homem-Aranha revelada, Peter pede ajuda ao Doutor Strange. Quando um feitiço corre mal, inimigos perigosos de outros mundos começam a aparecer, forçando Peter a descobrir o que realmente significa ser o Homem-Aranha."
+
+}
+
+function filme07() {
+    document.getElementById("titulo")
+        .innerHTML = "Halo";
+    document.getElementById("descricao")
+        .innerHTML = "Os extraterrestres ameaçam a existência humana em um confronto épico do século 26."
+
+}
+
+function filme08() {
+    document.getElementById("titulo")
+        .innerHTML = "Kingsman: O Círculo Dourado";
+    document.getElementById("descricao")
+        .innerHTML = "Quando sua sede é destroída e o mundo está em risco, os Kingsman descobrem uma organização de espionajem nos Estados Unidos e juntos acabaram com o enemigo comum."
+
+}
+
+function filme09() {
+    document.getElementById("titulo")
+        .innerHTML = "The Walking Dead";
+    document.getElementById("descricao")
+        .innerHTML = "Um Xerife acorda de um coma para dar-se conta que o mundo está em ruínas, e deve liderar um grupo de sobreviventes para se manter vivo."
+
+}
+
+function filme10() {
+    document.getElementById("titulo")
+        .innerHTML = "Caranguejo Negro";
+    document.getElementById("descricao")
+        .innerHTML = "Em um mundo pós-apocalíptico, seis soldados em uma missão secreta devem transportar um misterioso pacote através de um arquipélago gelado."
+
+}
+
+
+
+
+
+
+
 
 
 
