58d0d653df1ead0ec5e94782d2bf3075379902b4
AlessandroAraujo <alessandro.aoc@gmail.com>
2022-04-28 14:23:52 -0400

    pequena modificaçoes

--- js/lista.js
+++ js/lista.js
@@ -3,6 +3,8 @@
 function filme01() {
     document.getElementById('titulo')
         .innerHTML = "The Batman";
+
+    document.getElementById('img_banner').src = "img/FKC5caUVIAYKE4y-scaled.jpg"
     document.getElementById("descricao")
         .innerHTML = "Batman (The Batman, no original) segue o segundo ano de Bruce Wayne (Robert Pattinson) como o herói de Gotham, causando medo nos corações dos criminosos da sombria cidade."
 
@@ -12,6 +14,7 @@ function filme01() {
 function filme02() {
     document.getElementById("titulo")
         .innerHTML = "O Projeto Adam";
+    document.getElementById('img_banner').src = "img/maxresdefault.jpg"
     document.getElementById("descricao")
         .innerHTML = "Um viajante do tempo volta ao ano de 2022 por acidente e acaba se encontrando com seu eu mais novo. Juntos, e sendo caçados por forças do futuro, os dois embarcam em uma missão para consertar a linha temporal e salvar seus entes queridos."
 
