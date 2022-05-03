a8c7faf313dcb5df5dcf8a6765db41a74a3334e0
AlessandroAraujo <alessandro.aoc@gmail.com>
2022-05-03 10:54:06 -0400

    final

--- js/lista.js
+++ js/lista.js
@@ -1,59 +1,95 @@
 
+var listafav = new Array;
+
+
+
+
 function filme01() {
     document.getElementById('titulo')
         .innerHTML = "The Batman";
-
-    document.getElementById('bannerprincipal').style.backgroundImage=
-     "url(img/FKC5caUVIAYKE4y-scaled.jpg)";
-
-     document.getElementById("descricao")
+    document.getElementById('bannerprincipal').style.backgroundImage =
+        "url(img/FKC5caUVIAYKE4y-scaled.jpg)";
+    document.getElementById("descricao")
         .innerHTML = " Batman (The Batman, no original) segue o segundo ano de Bruce Wayne (Robert Pattinson) como o herói de Gotham, causando medo nos corações dos criminosos da sombria cidade."
-    
-    var bataman = document.getElementById('batman');
 
 }
 
+console.log(listafav);
+
+
+
 function filme02() {
-    document.getElementById('bannerprincipal').style.backgroundImage="url(img/Critica-Sem-Saida-830x450.jpg)";
+    document.getElementById('titulo')
+        .innerHTML = "Sem saida";
+    document.getElementById("descricao")
+        .innerHTML = " Em Sem Saida(No Exit) é um filme de suspense americano dirigido por Damien Power a partir de um roteiro de Andrew Barrer e Gabriel Ferrari, baseado no romance de 2017 de mesmo nome de Taylor Adams"
+
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(img/Critica-Sem-Saida-830x450.jpg)";
 }
 
 function filme03() {
-    var elemento = document.getElementByClassName("container");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Uncharted: fora do mapa";
+    document.getElementById("descricao")
+        .innerHTML = " Nathan Drake e seu parceiro canastrão Victor Sully Sullivan embarcam em uma perigosa busca para encontrar o maior tesouro jamais encontrado. Enquanto isso, eles também rastreiam pistas que podem levar ao irmão perdido de Nathan"
+
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://www.elhombre.com.br/wp-content/uploads/2021/10/uncharted.jpg)";
 }
 
 function filme04() {
-    var elemento = document.getElementsByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Projeto Adam";
+    document.getElementById("descricao")
+        .innerHTML = 'Um viajante do tempo volta ao ano de 2022 por acidente e acaba se encontrando com seu eu mais novo. Juntos, e sendo caçados por forças do futuro, os dois embarcam em uma missão para consertar a linha temporal e salvar seus entes queridos.'
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://minhaseriefavorita.com/wp-content/uploads/2022/03/o-projeto-adam-2.jpg)"
 }
 
 function filme05() {
-    var elemento = document.getElementsByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Bolha";
+    document.getElementById("descricao")
+        .innerHTML = " Durante a pandemia, um grupo de atores e a equipe de produção de um filme passam a quarentena juntos para gravar um longa de sucesso. Mas a convivência não é nada fácil e eles ainda precisam enfrentar a pressão do estúdio e um surto de gripe."
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://midias.agazeta.com.br/2022/04/01/710x388/filme-a-bolha-da-netflix-734842.jpg)"
 }
 
 function filme06() {
-    var elemento = document.getElementsByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Homem aranha sem volta para casa";
+    document.getElementById("descricao")
+        .innerHTML = "O Homem-Aranha precisa lidar com as consequências da sua verdadeira identidade ter sido descoberta."
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://odefensor.com.br/site/wp-content/uploads/2022/01/Homem-Aranha.jpeg)"
+
 }
 
 function filme07() {
-    var elemento = document.getElementByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Aguas Profundas";
+    document.getElementById("descricao")
+        .innerHTML = " Deep Water é um filme estadunidense de thriller psicológico erótico, dirigido por Adrian Lyne, baseado no romance de 1957 homônimo de Patricia Highsmith, a partir de um roteiro de Zach Helm e Sam Levinson."
+
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://i2.wp.com/www.bahiasocialvip.com.br/wp-content/uploads/2022/03/aguas-profundas-tv-pop.jpg?fit=639%2C426&ssl=1)"
 }
 
 function filme08() {
-    var elemento = document.getElementByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Um Jantarentre Espiões";
+    document.getElementById("descricao")
+        .innerHTML = " Dois amantes. Dois espiões. Um reencontro. Quem sairá vivo desse jantar? Em 2006, terroristas sequestraram um avião no Aeroporto de Viena, exigindo uma troca de reféns e determinados a não negociar. E, de fato, não negociaram "
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://i1.wp.com/www.geekguia.com.br/wp-content/uploads/2022/04/139085218.jpg?resize=970%2C570&ssl=1)"
 }
 
 function filme09() {
-    var elemento = document.getElementByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "O Commando";
+    document.getElementById("descricao")
+        .innerHTML = " Após uma missão fracassada, um agente de combate ao narcotráfico volta para casa traumatizado. Agora, ele tem que fazer de tudo para proteger sua família quando um criminoso esconde drogas em sua casa."
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://play-lh.googleusercontent.com/proxy/O0oFbjd9PRq1ZbuGRC0oeKrhUFrFhlHF1ik16_BnKAgERE3ALUFyHXN6eWJSexhv_aK3os50c9_5h_bMLdS7NALTxBST6o2QGqbarjeFyomrAYRD72qAUR1-qFVH7ciKZfTw1ymvvgXLMMZwPqfWI9Gsx4m_yqBhDd7Y5g=w720-h405-rw)"
 }
 
 function filme10() {
-    var elemento = document.getElementByClassName("filme-principal");
-    elemento.style.backgroundImage = 'img/FKC5caUVIAYKE4y-scaled.jpg'
+    document.getElementById('titulo')
+        .innerHTML = "Conta o Gelo";
+    document.getElementById("descricao")
+        .innerHTML = " Dois homens fazem uma arriscada expedição à Groenlândia em busca de um mapa perdido."
+    document.getElementById('bannerprincipal').style.backgroundImage = "url(https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABR6PsCKPbN6UrVi2XSOHSzpyHD1vcGMq2_53kS9R-kS3JRAOBIeV7ktlwxOG5QaacLvuHabNK7fmvxn_EwnAp8sNUQWB.jpg?r=19d)"
 }
-
